abf8d8897a42bb3baa659275c3cb7bcca4ff596b
NaoyaAramaki <n.aramaki29@gmail.com>
2022-10-16 16:49:07 +0900

    work15_sass開発環境構築

--- works/15/site/gulpfile.js
+++ works/15/site/gulpfile.js
@@ -1,21 +1,11 @@
-// gulpプラグインの読み込み
-const gulp = require("gulp");
-// Sassをコンパイルするプラグインの読み込み
-const sass = require("gulp-sass")(require("sass"));
-// ブラウザ同期
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const browserSync = require("browser-sync").create();
-
-// style.scssをタスクを作成する
-gulp.task("scss", () => {
-  // style.scssファイルを取得
-  return (
-    gulp
-      .src("/site/common/scss/style.scss")
-      // Sassのコンパイルを実行
-      .pipe(sass())
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("/site/common/"))
-  );
+//scssをcssに変換
+gulp.task('scss',() => {
+  return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css/'))
 });
 //リロードの監視用
 gulp.task('bs-reload', function(done) {
@@ -24,9 +14,9 @@ gulp.task('bs-reload', function(done) {
 });
 //ファイルの変更を監視
 gulp.task('watch',(done) => {
-  gulp.watch('/site/common/**/*.scss',gulp.series('scss','bs-reload'));
-  gulp.watch('/site/common/**/*.html',gulp.series('bs-reload'));
-  gulp.watch('/site/common/**/*.js',gulp.series('bs-reload'));
+  gulp.watch('**/*.scss',gulp.series('scss','bs-reload'));
+  gulp.watch('**/*.html',gulp.series('bs-reload'));
+  gulp.watch('**/*.js',gulp.series('bs-reload'));
   done();
 });
 //ブラウザシンク設定
